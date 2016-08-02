1a72387d23a1b70b74231ff3307d0ee89b3764f4
Regner Blok-Andersen <shadowdf@gmail.com>
2016-08-02 11:14:15 +0000

    Ensuring DB and table exist before first request

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ import rethinkdb as r
 
 from flask import Flask, jsonify, abort
 from flask_rethinkdb import RethinkDB
-from flask_restful import Resource, Api, fields, marshal_with, reqparse
+from flask_restful import Resource, Api, reqparse
+from rethinkdb.errors import ReqlOpFailedError
 # from eveauth.contrib.flask import authenticate
 
 app = Flask(__name__)
@@ -21,22 +22,16 @@ app.config['RETHINKDB_DB'] = os.environ.get('RDB_DB', 'test')
 
 RDB_TABLE = os.environ.get('RDB_TABLE', 'slack_webhooks')
 
-webhooks = {
-    'id': fields.String,
-    'name': fields.String,
-}
-
 
 class Webhooks(Resource):
     # @authenticate()
-    @marshal_with(webhooks)
     def get(self, character_id):
         # if request.token['character_id'] != character_id:
         #     abort(403)
 
         results = r.table(RDB_TABLE).filter(lambda row: row['character'].eq(character_id)).run(db.conn)
 
-        return list(results)
+        return [{'id': x['id'], 'name': x['name']} for x in results]
 
     # @authenticate()
     def post(self, character_id):
@@ -127,16 +122,26 @@ class WebhooksLookup(Resource):
             lambda row: row['ids'].default([]).contains(lambda id: r.expr(args['ids']).contains(id)).or_(row['value'].le(args['value']))
         ).pluck('url').distinct().run(db.conn)
 
-        return {'webhooks': [x['url'] for x in results]}
+        return [x['url'] for x in results]
 
 
-api.add_resource(Webhooks, '/api/settings/<int:character_id>/slack/webhooks/')
-api.add_resource(Webhook, '/api/settings/<int:character_id>/slack/webhooks/<string:webhook_id>/')
+api.add_resource(Webhooks, '/api/settings/slack/<int:character_id>/webhooks/')
+api.add_resource(Webhook, '/api/settings/slack/<int:character_id>/webhooks/<string:webhook_id>/')
 api.add_resource(WebhooksLookup, '/api/settings/slack/webhooks/lookup/')
 
 
 @app.before_first_request
 def setup_logging():
+    try:
+        r.db_create(app.config['RETHINKDB_DB']).run(db.conn)
+    except ReqlOpFailedError:
+        pass
+
+    try:
+        r.table_create(RDB_TABLE).run(db.conn)
+    except ReqlOpFailedError:
+        pass
+
     if not app.debug:
         app.logger.addHandler(logging.StreamHandler())
         app.logger.setLevel(logging.INFO)
